c6e08ed470c68e00aac2c4dd711d497ee3a086c4
Antoine Dumait <119426877+antoine-dumait@users.noreply.github.com>
2025-03-18 21:12:45 +0100

    Delete _build directory

--- _build/default/evaluateur.ml
+++
@@ -1,5 +0,0 @@
-let eval_expr _ = failwith "Not yet implemented <- Evaluateur.eval_expr"
-(* Syntax.expr ->  *)
-
-let eval_prog _ = failwith "Not yet implemented <- Evaluateur.eval_prog"
-(* Syntax.programme/Syntax.fun_decl list -> *)
