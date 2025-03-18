c4811cb19fe846c7ecc10fd1233593b1413d72c0
unknown <chameauq@gmail.com>
2025-03-18 15:31:31 +0100

    Verif.ml quasiement bon

--- verif.ml
+++ verif.ml
@@ -1,48 +1,94 @@
-type myTyp = Var | Fonc
-type valeur =   | Var of Syntax.idvar | IdFun of Syntax.idfun
-type truc = {
-  niveau: int;
-  id: valeur;
-  quoi: myTyp;
-  retour: Syntax.typ;
+type typeVar = {
+  idVar: string; (*non ?, devrait etre idvar | idfun ??*)  
+  valeur: Syntax.typ;
 }
 
-type env_type = truc list
+type typeFunc = {
+  idFun: string; (*non ?, devrait etre idvar | idfun ??*)  
+  params: Syntax.typ list;
+  valeur: Syntax.typ;
+}
 
-let add_var_type var_name _ = print_endline var_name
-(* let add_var_type var_name var_type = (var_name, var_type)::type_list *)
+type env_var_type = typeVar list
+type env_func_type = typeFunc list
+let bool_to_string b = if b then "true" else "false"
 
-
-(* Syntax.expr -> bool *)
-let verif_expr expr = match expr with
-(* | Syntax.Var var -> let _ = add_var_type var Syntax.TBool in true *)
-| Syntax.IdFun id_fun -> add_var_type id_fun Syntax.TBool; let _ = print_endline "in id_fun" in true
-| Syntax.Int i -> let _  = print_int i in true
-| _ -> let _ = if List.length type_list >= 1 then 
-                  print_endline (fst(List.hd type_list)) 
-               else print_endline "nothing"; print_int (List.length type_list); print_endline ""
-               in true
-
-
-(* | Syntax.IdFun expr -> true
-| Syntax.Int expr  -> true
-| Syntax.Bool expr -> true
-| Syntax.BinaryOp expr -> true
-| Syntax.UnaryOp expr  -> true
-| Syntax.If expr  -> true
-| Syntax.Let expr  -> true
-| Syntax.App expr  -> true *)
-  (* let _ = print_endline (Syntax.string_of_expr expr) in true *)
+(* Syntax.expr -> Syntax.typ -> env_var_type -> bool *)
+let rec verif_expr expr voulu envVar envFun = match expr with
+| Syntax.Var id -> let rec aux env = 
+                      (match env with  
+                        | [] -> false 
+                        | t::reste -> if t.idVar = id then 
+                          (if t.valeur = voulu then 
+                            true 
+                          else 
+                            false) 
+                        else 
+                          aux reste )
+                    in aux envVar
+| Syntax.IdFun id -> let rec aux env = 
+  (match env with  
+    | [] -> false 
+    | t::reste -> if t.idFun = id then 
+      (if t.valeur = voulu then 
+        true 
+      else 
+        false) 
+    else 
+      aux reste )
+in aux envFun
+ |Syntax.Int n -> print_int n; print_endline " int"; voulu = TInt
+| Syntax.Bool b -> print_string (bool_to_string b); print_endline "bool"; voulu = TBool
+| Syntax.UnaryOp (_, expr1) -> print_endline "unaryOp"; verif_expr expr1 TBool envVar envFun 
+| Syntax.BinaryOp (op, expr1, expr2) -> print_string "binaryOp "; if (op = And || op = Or) then 
+                                                                    let _ = print_endline "TBool" in verif_expr expr1 TBool envVar envFun && verif_expr expr2 TBool envVar envFun 
+                                                                  else
+                                                                    let _ = print_endline "TInt" in verif_expr expr1 TInt envVar envFun && verif_expr expr2 TInt envVar envFun 
+| Syntax.If (b, expr1, expr2) -> print_endline "if"; verif_expr b TBool envVar envFun && 
+                                (verif_expr expr1 TBool envVar envFun && verif_expr expr2 TBool envVar envFun) &&
+                                (verif_expr expr1 TInt envVar envFun && verif_expr expr2 TInt envVar envFun) 
+| Syntax.Let (idd, ty, exprIn, exprOut) -> print_endline "let"; if verif_expr exprIn ty envVar envFun then let a = {idVar=idd; valeur=ty}::envVar in verif_expr exprOut voulu a envFun else false
+|Syntax.App (funId, exprList) ->  print_endline "app";let rec aux env = 
+  (match env with  
+    | [] -> false 
+    | t::reste -> if t.idFun = funId then 
+      (if t.valeur = voulu then
+        let rec aux p l= (match (p, l) with 
+          | ([],[]) -> true
+          | ([],_) -> false (* une liste vide mais pas lautre*)
+          | (_,[]) -> false
+          | (typeParam::resteParams, expr::resteExpr) -> verif_expr expr typeParam envVar envFun && aux resteParams resteExpr
+          ) in aux t.params exprList
+      else 
+        false) 
+    else 
+      aux reste)
+in aux envFun
 
 (* Syntax.fun-decl -> bool *)
 (* TODO: demander prof si il faut declarer le type d'entrée*)
-let verif_decl_fun (fonction: Syntax.fun_decl) = let _ = print_endline fonction.id 
-  in 
-verif_expr fonction.corps
-
-(* Syntax.programme/Syntax.fun_decl list -> bool *)
+let verif_decl_fun (fonction: Syntax.fun_decl) envVar envFun = 
+ let b = verif_expr fonction.corps fonction.typ_retour envVar envFun
+in let _ = print_endline (Syntax.string_of_fun_decl fonction)
+in let _ = print_string fonction.id 
+in let _ = print_string "=" 
+in let _ = print_endline (bool_to_string b)
+in b 
+(* Syntax.programme/Syntax.fun_ decl list -> bool *)
 let verif_prog prog = 
-  let rec aux prog = match prog with 
+  let _ = print_string (Syntax.string_of_programme prog) in
+  let rec verifFunctions prog = match prog with 
   | [] -> true
-  | fonction::reste -> if verif_decl_fun fonction then aux reste else false
-  in aux prog
+  | fonction::reste -> if fonction.Syntax.id = "main" then verifFunctions reste else verif_decl_fun fonction [] [] in 
+  let functionsTypeForMain = 
+    let rec second p = match p with 
+      | [] -> []
+      | (_, typ)::reste -> typ::(second reste)
+    in let rec aux funcDeclList = match funcDeclList with
+      | [] -> []
+      | funDec::reste -> {idFun=funDec.Syntax.id; params=(second funDec.Syntax.var_list); valeur=funDec.Syntax.typ_retour}::(aux reste)
+    in aux prog in         
+  let rec verifMain prog = match prog with 
+  | [] -> false
+  | fonction::reste -> if fonction.Syntax.id = "main" then verif_decl_fun fonction [] functionsTypeForMain else verifMain reste
+  in if verifFunctions prog then verifMain prog else false
